01d6a292c06c3bfb30daebcf22b36c4b7f7571c2
Dargin Mathi <darginmathi@gmail.com>
2025-03-17 11:23:10 +0000

    added start and end openings

--- maze.py
+++ maze.py
@@ -1,9 +1,10 @@
 from cell import Cell
 import time
+import random
 
 
 class Maze:
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None, seed=None):
         self._cells = []
         self._x1 = x1
         self._y1 = y1
@@ -12,6 +13,9 @@ class Maze:
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
+        
+        if seed is not None:
+            random.seed(seed)
         
         self._create_cells()
         
@@ -22,6 +26,9 @@ class Maze:
                 cell = Cell(self._win)
                 column.append(cell)
             self._cells.append(column)
+            
+        self._break_entrance_and_exit()
+            
         for i in range(self._num_cols):
             for j in range(self._num_rows):
                 self._draw_cell(i, j)
@@ -43,3 +50,7 @@ class Maze:
             return
         self._win.redraw()
         time.sleep(0.05)
+        
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._cells[-1][-1].has_bottom_wall = False
