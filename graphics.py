5e5e4d77ed6ff631ffea76bdd6837668c6982cc1
Dargin Mathi <darginmathi@gmail.com>
2025-03-18 11:25:59 +0000

    added break walls recurcive method but its not breaking walls or updating after breaking them so need to look into this later

--- graphics.py
+++ graphics.py
@@ -9,19 +9,23 @@ class Window:
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
         
-    def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
-        
     def wait_for_close(self):
         self.__running = True
         while self.__running:
             self.redraw()
         print("window closed")
-        
+    
+    # helper method for wait_for_close()
+    
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+            
     def draw_line(self, line, fill_colour="black"):
         line.draw(self.__canvas, fill_colour)
-            
+
+    # used in __init__
+    
     def close(self):
         self.__running = False
         
@@ -35,6 +39,8 @@ class Line:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
+    
+    # helper method for draw_line()
     
     def draw(self, canvas, fill_colour="black"):
         canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_colour, width=2)
--- maze.py
+++ maze.py
@@ -4,7 +4,7 @@ import random
 
 
 class Maze:
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None, seed=None):
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None, seed=None): # x1, y1 == Margin
         self._cells = []
         self._x1 = x1
         self._y1 = y1
@@ -14,11 +14,18 @@ class Maze:
         self._cell_size_y = cell_size_y
         self._win = win
         
-        if seed is not None:
+        if seed:
             random.seed(seed)
+        
+        # calls to create cells when a Maze object is created
         
         self._create_cells()
         
+        self._break_walls_r(0, 0)
+                
+        self._reset_visited()
+                
+                
     def _create_cells(self):
         for i in range(self._num_cols):
             column = []
@@ -33,6 +40,73 @@ class Maze:
             for j in range(self._num_rows):
                 self._draw_cell(i, j)
             
+    # will be used in _create_cells() to create entry and exit.
+        
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._cells[-1][-1].has_bottom_wall = False
+    
+    
+    # create a random Maze    
+        
+    def _break_walls_r(self, i, j):
+        self._cells[i][j].visited = True
+        while True:
+            to_visit = []
+            
+            # adds unvisited cells to list
+            
+            if i+1 < self._num_cols and not self._cells[i+1][j].visited:
+                to_visit.append([i+1, j])
+            if i-1 >= 0 and not self._cells[i-1][j].visited:
+                to_visit.append([i-1, j])
+            if j+1 < self._num_rows and not self._cells[i][j+1].visited:
+                to_visit.append([i, j+1])
+            if j-1 >= 0 and not self._cells[i][j-1].visited:
+                to_visit.append([i, j-1])
+
+            # break if no where to go
+
+            if not to_visit:
+                self._draw_cell(i, j)
+                return
+            
+            # rng choose where to go
+            
+            rng = random.choice(to_visit)
+            k, l = rng[0], rng[1]
+            
+            # break walls by checking the direction
+            
+            if i > k:
+                # left
+                self._cells[i][j].has_left_wall = False
+                self._cells[k][l].has_right_wall = False
+            elif i < k:
+                # right
+                self._cells[i][j].has_right_wall = False
+                self._cells[k][l].has_left_wall = False
+            elif j > l:
+                # bot
+                self._cells[i][j].has_bottom_wall = False
+                self._cells[k][l].has_top_wall = False
+            elif j < l:
+                #top
+                self._cells[i][j].has_top_wall = False
+                self._cells[k][l].has_bottom_wall = False
+            
+            self._draw_cell(i, j)
+            # recursion
+                
+            self._break_walls_r(k, l)
+            
+    def _reset_visited(self):
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._cells[i][j].visited = False        
+            
+    # helper method used in create_cells()
+    
     def _draw_cell(self, i, j):
         if self._win is None:
             return
@@ -51,6 +125,3 @@ class Maze:
         self._win.redraw()
         time.sleep(0.05)
         
-    def _break_entrance_and_exit(self):
-        self._cells[0][0].has_top_wall = False
-        self._cells[-1][-1].has_bottom_wall = False
